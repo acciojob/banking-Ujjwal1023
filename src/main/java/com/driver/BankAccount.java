75a0fcf363441d385bcd7d565bc9c5a6c37e4488
UJJWAL'S_WORLD <kumarujjwal471@gmail.com>
2023-06-17 23:29:51 +0530

    2nd spring assignment

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -14,18 +14,38 @@ public class BankAccount {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-
-        return null;
+       StringBuilder sb=new StringBuilder();
+       for(int i=0;i<digits;i++){
+           if(sum>=9){
+               sb.append(9);
+               sum-=9;
+           }
+           else{
+               sb.append(sum);
+               sum=0;
+           }
+       }
+       if(sum>0){
+           throw new Exception("Account number cannot be generated");
+       }
+        return sb.toString();
     }
 
     public void deposit(double amount) {
         //add amount to balance
+        this.balance+=amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+      double remainBalance=this.balance-amount;
+      if(remainBalance<this.minBalance){
+          throw new Exception("Insufficient Balance");
+      }
+      else{
+          this.balance-=amount;
+      }
     }
 
 }
